a844ac39eeae40b2400ae9e6ae3c75579f0f3670
johmatos <contato.johnatan06@gmail.com>
2022-05-10 04:38:32 +0200

    teste

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 03:02:41 by johmatos          #+#    #+#             */
-/*   Updated: 2022/05/09 21:11:30 by coder            ###   ########.fr       */
+/*   Updated: 2022/05/09 22:54:26 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,22 @@
 
 t_size	ft_strlcat(char *dst, const char *src, t_size size)
 {
+	char	*ft;
+	int		lenght;
+
+	lenght = ft_strlen(src);
+	ft = dst;
 	while (size-- != 0 && *dst != '\0')
+	{
 		dst++;
-		
+	}
+	while (size-- != 0 && *src)
+	{
+		*dst = *src;
+		src++;
+		dst++;
+	}
+	dst = ft;
+	*dst = '\0';
+	return (size - lenght);
 }
